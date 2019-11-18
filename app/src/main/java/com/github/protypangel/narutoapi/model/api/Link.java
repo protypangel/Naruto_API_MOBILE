4dd15f3100d836e1814d5f75b26be6cf31dc7ce4
protypangel <mascret@et.esiea.fr>
2019-11-18 14:07:57 +0100

    Changer toute la structure pour que cela soit du MVC. Changement des images + utilisation d'une database (PHPMYADMIN)

--- app/src/main/java/com/github/protypangel/narutoapi/model/api/Link.java
+++ app/src/main/java/com/github/protypangel/narutoapi/model/api/Link.java
@@ -4,6 +4,6 @@ public class Link {
     public static String api         = "http://ns202518.ovh.net/mehdi/api/naruto/";
     public static String logo        = "http://ns202518.ovh.net/mehdi/api/naruto/image/other/logo.png";
     public static String image       = "http://ns202518.ovh.net/mehdi/api/naruto/image/";
-    public static String capacité    = "http://ns202518.ovh.net/mehdi/api/naruto/image/other/capacite.png";
+    public static String capacite    = "http://ns202518.ovh.net/mehdi/api/naruto/image/other/capacite.png";
     public static String description = "http://ns202518.ovh.net/mehdi/api/naruto/image/other/description.png";
 }
